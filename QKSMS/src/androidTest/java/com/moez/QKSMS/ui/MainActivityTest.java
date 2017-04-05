e65d890e65f94b0149ecd6bc90dfa98bb12f90e8
Alex Land <aland@sfu.ca>
2017-04-04 19:24:43 -0700

    Added UI tests for creating new conversations, replying to existing conversations

---
+++ QKSMS/src/androidTest/java/com/moez/QKSMS/ui/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.moez.QKSMS.ui;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.moez.QKSMS.ui.settings.SettingsFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * Base test class for tests beginning in the Main Activity. Provides a setUp method that disables
+ * the Welcome screen and MMS setup dialogs.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
+
+    @Before
+    public void setUp() throws Exception {
+        SharedPreferences.Editor sharedPreferences = PreferenceManager.getDefaultSharedPreferences(InstrumentationRegistry.getTargetContext()).edit();
+        sharedPreferences.putBoolean(SettingsFragment.WELCOME_SEEN, true);
+        sharedPreferences.putBoolean(MainActivity.MMS_SETUP_DONT_ASK_AGAIN, true);
+        sharedPreferences.commit();
+        mActivityTestRule.launchActivity(null);
+    }
+}
---
+++ QKSMS/src/androidTest/java/com/moez/QKSMS/ui/ReplyExistingConversationTest.java
@@ -0,0 +1,48 @@
+package com.moez.QKSMS.ui;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.moez.QKSMS.R;
+
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+public class ReplyExistingConversationTest extends MainActivityTest{
+
+    @Test
+    public void replyExistingConversationTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.conversations_list), isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        long timestamp = System.currentTimeMillis();
+        String messageString = "Another Message " + timestamp;
+
+        ViewInteraction qKEditText = onView(
+                allOf(withId(R.id.compose_reply_text), isDisplayed()));
+        qKEditText.perform(replaceText(messageString), closeSoftKeyboard());
+
+        ViewInteraction frameLayout = onView(
+                allOf(withId(R.id.compose_button), isDisplayed()));
+        frameLayout.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withText(messageString),
+                        isDisplayed()));
+        textView.check(matches(withText(messageString)));
+
+    }
+}
